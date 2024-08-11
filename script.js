38bad832a406daeccbe54d4fb5251addba281262
JanhviVerma <janhvi.verma2509@gmail.com>
2024-08-11 10:43:46 +0000

    Expanded functionality by adding images to the recipe cards

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('Food Recipe Book is ready to go!');
+    const recipeButtons = document.querySelectorAll('.view-recipe');
+    
+    recipeButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            alert('Recipe details will be displayed here soon!');
+        });
+    });
+    
+    console.log('Breakfast and lunch recipes are now interactive.');
 });
